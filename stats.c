8094d484a8877bac0f878c8fca972ba3c140a3a7
Vlad Cioaba <vlad.cioaba@gmail.com>
2020-01-26 16:17:18 +0200

    Adding code outline and documentation for C1M1 Assessment

--- stats.c
+++ stats.c
@@ -8,17 +8,15 @@
  * misuse of this material. 
  *
  *****************************************************************************/
+
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file stats.c
+ * @brief Implementation of some utility functions for various statistic calculations.
  *
- * <Add Extended Description Here>
- *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author Vlad Cioaba
+ * @date 26/01/2020
  *
  */
-
 
 
 #include <stdio.h>
@@ -27,7 +25,98 @@
 /* Size of the Data Set */
 #define SIZE (40)
 
-void main() {
+void print_statistics(unsigned char arr[], unsigned int len) {
+  printf("initial array:\n");
+  print_array(arr, len);
+  printf("\n");
+  printf("maximum: %d\n", find_maximum(arr, len));
+  printf("minimum: %d\n", find_minimum(arr, len));
+  printf("\n");
+  sort_array(arr, len);
+  printf("sorted array:\n");
+  print_array(arr, len);
+  printf("\n");
+  printf("median: %f\n", find_median(arr, len));
+  printf("mean: %f\n", find_mean(arr, len));
+}
+
+void print_array(unsigned char arr[], unsigned int len) {
+  if (len) {
+    const unsigned short maxrow = len+1;
+    for (unsigned int i = 0; i < len; ++i) {
+      if ((i+1) % maxrow == 0) {
+        printf("\n");
+      }
+      printf("%i", arr[i]);
+      if (i < len - 1) {
+        printf(",");
+      }
+    }
+  }
+}
+
+double find_median(unsigned char arr[], unsigned int len) {
+  double median = 0;
+  if (len) {
+    if (len % 2 == 0) {
+      median = (arr[(len-1)/2] + arr[(len-1)/2])/2.f;
+    } else {
+      median = arr[len/2];
+    }
+  }
+  return median;
+}
+
+double find_mean(unsigned char arr[], unsigned int len) {
+  if (len) {
+    unsigned long sum = 0;
+    for (unsigned int i = 0; i < len; ++i) {
+      sum += arr[i];
+    }
+    return sum / (double)len;
+  }
+  return 0;
+}
+
+unsigned char find_maximum(unsigned char arr[], unsigned int len) {
+  unsigned char maxc = 255;
+  if (len) {
+    for (unsigned int i = 0; i < len; ++i) {
+      if (maxc > arr[i]) {
+        maxc = arr[i];
+      }
+    }
+  }
+  return maxc;
+}
+
+unsigned char find_minimum(unsigned char arr[], unsigned int len) {
+  if (len) {
+    unsigned char minc = 0;
+    for (unsigned int i = 0; i < len; ++i) {
+      if (minc < arr[i]) {
+        minc = arr[i];
+      }
+    }
+    return minc;
+  }
+  return 0;
+}
+
+void sort_array(unsigned char arr[], unsigned int len) {
+  // simple bubblesort
+  for (unsigned int i = 0; i < len; ++i) {
+    for (unsigned int j = 0; j < len-1; ++j) {
+      if (arr[j]<arr[j+1]) {
+        char t = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = t;
+      }
+    }
+  }
+}
+
+int main() {
 
   unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
                               114, 88,   45,  76, 123,  87,  25,  23,
@@ -35,9 +124,7 @@ void main() {
                               201,   6,  12,  60,   8,   2,   5,  67,
                                 7,  87, 250, 230,  99,   3, 100,  90};
 
-  /* Other Variable Declarations Go Here */
-  /* Statistics and Printing Functions Go Here */
+  print_statistics(test, SIZE);
 
+  return 0;
 }
-
-/* Add other Implementation File Code Here */
--- stats.h
+++ stats.h
@@ -8,33 +8,94 @@
  * misuse of this material. 
  *
  *****************************************************************************/
+
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file stats.h
+ * @brief Utility functions for various statistics calculations.
  *
- * <Add Extended Description Here>
- *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author Vlad Cioaba
+ * @date 26/01/2020
  *
  */
+
 #ifndef __STATS_H__
 #define __STATS_H__
 
-/* Add Your Declarations and Function Comments here */ 
-
 /**
- * @brief <Add Brief Description of Function Here>
+ * @brief A function that prints the statistics of an array including minimum, maximum, mean, and median.
  *
- * <Add Extended Description Here>
+ * @param unsigned char[] 	the array
+ * @param unsigned int 		length of the array
  *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- *
- * @return <Add Return Informaiton here>
  */
 
+void print_statistics(unsigned char arr[], unsigned int len);
+
+/**
+ * @brief Given an array of data and a length, prints the array to the screen.
+ *
+ * @param unsigned char[] 	the array
+ * @param unsigned int 		length of the array
+ *
+ */
+
+void print_array(unsigned char arr[], unsigned int len);
+
+/**
+ * @brief Given an array of data and a length, returns the median value.
+ *
+ * @param unsigned char[] 	the array
+ * @param unsigned int 		length of the array
+ *
+ * @return double 	median value
+ */
+
+double find_median(unsigned char arr[], unsigned int len);
+
+/**
+ * @brief Given an array of data and a length, returns the mean value.
+ *
+ * @param unsigned char[] 	the array
+ * @param unsigned int 		length of the array
+ *
+ * @return double 	mean value
+ */
+
+double find_mean(unsigned char arr[], unsigned int len);
+
+/**
+ * @brief Given an array of data and a length, returns the maximum value.
+ *
+ * @param unsigned char[] 	the array
+ * @param unsigned int 		length of the array
+ *
+ * @return unsigned char 	maximum value
+ */
+
+unsigned char find_maximum(unsigned char arr[], unsigned int len);
+
+/**
+ * @brief Given an array of data and a length, returns the minimum value.
+ *
+ * @param unsigned char[] 	the array
+ * @param unsigned int 		length of the array
+ *
+ * @return unsigned char 	minimum value
+ */
+
+unsigned char find_minimum(unsigned char arr[], unsigned int len);
+
+/**
+ * @brief Given an array of data and a length, sorts the array from largest to smallest.
+ *
+ *  The zeroth Element should be the largest value, and the last element (n-1) should be the smallest value.
+ *
+ *
+ * @param unsigned char[] 	the array
+ * @param unsigned int 		length of the array
+ *
+ */
+
+void sort_array(unsigned char arr[], unsigned int len);
 
 #endif /* __STATS_H__ */
